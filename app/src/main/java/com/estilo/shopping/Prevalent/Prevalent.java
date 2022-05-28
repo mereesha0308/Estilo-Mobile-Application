fa679d1721c387a7f2dd574ff493e81255821ab0
“mereesha0308” <mereeshanavodani0308@gmail.com>
2022-05-28 12:35:43 +0530

    First commit

---
+++ app/src/main/java/com/estilo/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.estilo.shopping.Prevalent;
+
+import com.estilo.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
